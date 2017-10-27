85cd315f753f3ea38339a298166c155f819fe422
Pamella <pamella108@gmail.com>
2017-10-27 03:47:49 +0000

    ex 02

---
+++ lista03/02.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tamanho (char *string)
+{
+	int acum=0, ind=0;
+
+	while (string[ind] != '\0') {
+		acum++;
+		ind++;
+	}
+	return acum-1;
+}
+
+char *invertida (char *string)
+{
+	int tam = tamanho(string), ind = tamanho(string);
+	char *invertida = malloc(sizeof (char)*265);
+
+	for (int cont = 0; cont<=tam; cont++) {
+		invertida[cont] = string[ind];
+		ind--;
+	}
+	return invertida;
+}
+
+char main () 
+{
+	char string[265];
+	
+
+	printf("Digite algo: ");
+	scanf("%s", string);
+
+	printf("invertida: %s\n", invertida(string));
+}
