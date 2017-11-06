88937122f7b5ff2f2cb9b8d99baf0170eab5ca75
Pamella <pamella108@gmail.com>
2017-11-06 04:26:43 +0000

    update

--- lista03/03.c
+++ lista03/03.c
@@ -8,28 +8,30 @@ int *soma (int *v1, int *v2)
 	for(int ind=0; ind<5; ind++) {
 		v3[ind] = v1[ind] + v2[ind];
 	}
-
-	for(int ind=0; ind<5; ind++) {
-		printf("%d ", v3[ind]);
-	}
+	
+	return v3;
 }
 
 int main() 
 {
-	int v1[5], v2[5], ind=0;
+	int v1[5], v2[5], ind=0, *v3;
 
 
 	for(ind=0; ind<5; ind++) {
 		
-		scanf("%d", &v2[ind]);
+		scanf("%d", (v2+ind));
 		
 	}
 
 	
 	for(ind=0; ind<5; ind++) {
-		printf("vetor 1: ");
+
 		scanf("%d", &v1[ind]);
 	}
 
-	soma(v1,v2);
+	v3 = soma(v1, v2);
+
+	for (int i =0; i<5; i++) {
+		printf("%d ", v3[i]);
+	} 
 }
